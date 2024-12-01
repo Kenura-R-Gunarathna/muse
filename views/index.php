55de577d8218b0f3db00e6da1a046275130a45eb
Kenura-R-Gunarathna <kenuragunarathna@gmail.com>
2024-12-01 17:46:19 +0530

    updating index page

--- views/index.php
+++ views/index.php
@@ -6,6 +6,5 @@
     <li><a href="<?= route("index"); ?>">Home page</a></li>
     <li><a href="<?= route("admin.login") ?>">Admin login page</a></li>
     <li><a href="<?= route("admin.signup") ?>">Admin signup page</a></li>
-    <li><a href="<?= route("contact") ?>">Contact page</a></li>
     </li>
 </ul>
